c9c77dd6d8dada49adcaf4f0c339183c8dc5895f
be4n01 <107491716+be4n01@users.noreply.github.com>
2022-07-26 18:54:06 +0300

    Code Started

---
+++ Form1/Form1/Filler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Form1.Model
+{
+    class Filler
+    {
+        static private List<Filler> Fill = new List<Filler>();
+        public int Id { get; set; }
+        public string Date { get; set; }
+        public int InvNum { get; set; }
+        public string ObjName { get; set; }
+        public int Count { get; set; }
+        public float Price { get; set; }
+
+        public void save()
+        {
+            Fill.Add(this);
+        }
+
+        static public List<Filler> GetAllProducts()
+        {
+            return Fill;
+        }
+
+
+    }
+}
